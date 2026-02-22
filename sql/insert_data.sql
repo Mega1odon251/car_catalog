46a96e15f77132b0437b9a960cb1b89f6b4c2660
Megalodon <vlad.krupenko.00@bk.ru>
2026-02-22 04:30:51 +0300

    feat: add 10 more car records for analysis

---
+++ sql/insert_data.sql
@@ -0,0 +1,12 @@
+INSERT INTO cars (brand, model, year, price) VALUES
+('Ford', 'Focus', 2019, 850000),
+('Hyundai', 'Sonata', 2021, 2100000),
+('Kia', 'Sportage', 2022, 2800000),
+('Mercedes', 'E-Class', 2020, 4500000),
+('Audi', 'Q5', 2023, 5200000),
+('Volkswagen', 'Passat', 2021, 2300000),
+('Nissan', 'Qashqai', 2022, 2600000),
+('Toyota', 'RAV4', 2023, 3300000),
+('BMW', 'X3', 2021, 4100000),
+('Lada', 'Vesta', 2024, 1200000);
+SELECT COUNT(*) FROM cars;
